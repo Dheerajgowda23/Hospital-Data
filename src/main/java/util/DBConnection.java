ae9de945e12b5f98e9d3f8e73b5a950431893726
Dheerajgowda23 <dheerajb262@gmail.com>
2026-05-15 23:26:18 +0530

    supabase setup

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -7,14 +7,11 @@ public class DBConnection {
 
     public static Connection getConnection() throws Exception {
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("org.postgresql.Driver");
 
         return DriverManager.getConnection(
-
             System.getenv("DB_URL"),
-
             System.getenv("DB_USER"),
-
             System.getenv("DB_PASSWORD")
         );
     }
